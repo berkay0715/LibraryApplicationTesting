fcaf687b2c2a0cc3b99e2e91f71c4c431c512c5b
Berkay <celib181_aehit@students.vizja.pl>
2022-10-25 19:05:34 +0200

    FrameWork

--- src/test/java/com/FrameWork/runners/CukesRunner.java
+++ src/test/java/com/FrameWork/runners/CukesRunner.java
@@ -7,9 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                "pretty",
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json",
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
